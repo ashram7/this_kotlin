2f8da485102c3c875dfe6c5231f729f3f65254e2
ashram7 <ashram7@hanmail.net>
2022-10-11 14:15:05 +0900

    chapter function

--- BasicSyntax/app/src/main/java/com/ashram7/basicsyntax/MainActivity.kt
+++ BasicSyntax/app/src/main/java/com/ashram7/basicsyntax/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var myName = "홍길동"
+        val myName = "홍길동"
         var myAge : Int
         myAge = 27
         myAge += 1
