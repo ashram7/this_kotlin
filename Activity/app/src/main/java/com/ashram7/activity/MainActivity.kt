3e6ede8c865574322dd5a69cd5ac67f3a90ceb2f
ashram7 <ashram7@hanmail.net>
2022-10-18 13:00:55 +0900

    activity uploaded

--- Activity/app/src/main/java/com/ashram7/activity/MainActivity.kt
+++ Activity/app/src/main/java/com/ashram7/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ashram7.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.ashram7.activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // SubActivity 에서 돌려준 값을 받는 코드
+        val activityResult = registerForActivityResult(
+            ActivityResultContracts
+            .StartActivityForResult()){
+            // resultCode 코드가 정상인지 체크하는 코드
+            if (it.resultCode == RESULT_OK){
+                // 돌려받은 인텐트에서 메세지를 꺼내 변수를 저장
+                val message = it.data?.getStringExtra("returnValue")
+                // 해당하는 메세지를 화면에 보여주는 코드
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }
 
         val intent = Intent(this, SubActivity::class.java)
         intent.putExtra("from1", "Hello Bundle")
