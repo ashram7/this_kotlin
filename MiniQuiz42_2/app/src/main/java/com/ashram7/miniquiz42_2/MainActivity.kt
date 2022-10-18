3e3ec3532f823a4d574bf25a80e8b483e6e878a6
ashram7 <ashram7@hanmail.net>
2022-10-18 10:57:05 +0900

    mission 42-3

--- MiniQuiz42_2/app/src/main/java/com/ashram7/miniquiz42_2/MainActivity.kt
+++ MiniQuiz42_2/app/src/main/java/com/ashram7/miniquiz42_2/MainActivity.kt
@@ -15,10 +15,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
+        binding.checkApple.setOnCheckedChangeListener { buttonView, isChecked ->
+            apple = isChecked
+            printCheckedItems()
+        }
+        binding.checkBanana.setOnCheckedChangeListener { buttonView, isChecked ->
+            banana = isChecked
+            printCheckedItems()
+        }
+        binding.checkOrange.setOnCheckedChangeListener { buttonView, isChecked ->
+            orange = isChecked
+            printCheckedItems()
+        }
     }
 
     fun printCheckedItems(){
+        var result = ""
+        if (apple) result = " 사과"
 
+        if (banana) {
+            if (result.isNotEmpty()) result += "와"
+            result += " 바나나"
+        }
+        if (orange) {
+            if (result.isNotEmpty()) result += "와"
+            result += " 오렌지"
+        }
+        if (result.isNotEmpty()) {
+            binding.textView.text = "${result}가 선택되었습니다."
+        } else {
+            binding.textView.text = "선택되지 않았습니다."
+        }
     }
 }
