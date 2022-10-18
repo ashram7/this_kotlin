8b3831caa6dcd69bb9b62a2b5f22ee9617ffb3ab
ashram7 <ashram7@hanmail.net>
2022-10-18 12:51:21 +0900

    activity uploaded

---
+++ Activity/app/src/main/java/com/ashram7/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ashram7.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ashram7.activity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val intent = Intent(this, SubActivity::class.java)
+        intent.putExtra("from1", "Hello Bundle")
+        intent.putExtra("from2", 2022)
+        binding.btnStart.setOnClickListener {startActivity(intent)}
+    }
+}
