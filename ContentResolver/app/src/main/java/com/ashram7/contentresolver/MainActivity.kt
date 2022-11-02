94f2fb73aa0edeecaed2610bc75b39785f1e426c
ashram7 <ashram7@hanmail.net>
2022-11-02 14:36:20 +0900

    ContentResolver player list

--- ContentResolver/app/src/main/java/com/ashram7/contentresolver/MainActivity.kt
+++ ContentResolver/app/src/main/java/com/ashram7/contentresolver/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ashram7.contentresolver
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -9,25 +10,25 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ashram7.contentresolver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     lateinit var storagePermission: ActivityResultLauncher<String>
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    override fun permissionGranted(requestCode: Int) {
+        startProcess()
+    }
+
+    override fun permissionDenied(requestCode: Int) {
+        Toast.makeText(this
+            , "외부저장소 권한 승인이 필요합니다. 앱을 종료합니다."
+            , Toast.LENGTH_LONG)
+            .show()
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        storagePermission = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()) { isGranted ->
-            if(isGranted) {
-                startProcess()
-            } else {
-                Toast.makeText(baseContext,
-                    "외부 저장소 권한을 승인해야 앱을 사용할 수 있습니다.",
-                    Toast.LENGTH_LONG).show()
-                finish()
-            }
-        }
+        requirePermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 999)
     }
 
     fun startProcess() {
