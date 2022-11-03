9f8a6368c44f1beb632b32fbc16f1583cb5bec20
ashram7 <ashram7@hanmail.net>
2022-11-03 11:53:42 +0900

    google map

--- GoogleMaps/app/src/main/java/com/ashram7/googlemaps/MapsActivity.kt
+++ GoogleMaps/app/src/main/java/com/ashram7/googlemaps/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.ashram7.googlemaps
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,6 +13,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.ashram7.googlemaps.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -30,10 +35,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val LATLNG = LatLng(37.566418, 126.977943)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(37.398741, 126.921039)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val cameraPosition = CameraPosition.Builder()
+            .target(LATLNG)
+            // 줌 레벨이 높을수록 더 자세한 지도를 볼 수 있음
+            .zoom(15.0f)
+            .build()
+        //지도에서 사용할 수 있는 카메라 정보가 생성
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        // 카메라 포지션을 기준으로 지도의 위치, 배율, 기울기 등이 변경되서 표시
+        mMap.moveCamera(cameraUpdate)
+
+        var bitmapDrawable: BitmapDrawable
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bitmapDrawable = getDrawable(R.drawable.marker) as BitmapDrawable
+        } else {
+            bitmapDrawable = resources.getDrawable(R.drawable.marker) as BitmapDrawable
+        }
+        // 마커의 크기 조절
+        var scaledBitmap = Bitmap.createScaledBitmap( bitmapDrawable.bitmap, 50, 50, false)
+        // BitmapDescriptorFactory.fromBitmap() 메서드에 BitmapDrawable의 비트맵 객체를
+        // 전달하는 마커 아이콘을 위한 BitmapDescriptor 객체를 생성하고 import 해준다.
+        var discriptor = BitmapDescriptorFactory.fromBitmap(scaledBitmap)
+
+        // 마커를 추가한다. 마커의 옵션을 정의한 MarkerOptions() 객체가 필요
+        val markerOptions = MarkerOptions()
+            .position(LATLNG)
+            .title("Seoul City Hall")
+            .snippet("37.566418, 126.977943")
+            .icon(discriptor)
+
+        // GoogleMap 객체의 addMarker() 메서드에 MarkerOptions를 전달하면 구글 지도에 마커가 추가됨
+        mMap.addMarker(markerOptions)
     }
 }
