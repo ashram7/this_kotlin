f1f6bbd45801b9eb4f03318a101ce9ae789b24ae
ashram7 <ashram7@hanmail.net>
2022-11-04 12:42:23 +0900

    Libraries program

---
+++ SeoulPublicLibraries/app/src/main/java/com/ashram7/seoulpubliclibraries/SeoulOpenApi.kt
@@ -0,0 +1,18 @@
+package com.ashram7.seoulpubliclibraries
+
+import data.Library
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+class SeoulOpenApi {
+    companion object {
+        val DOMAIN = "http://openAPI.seoul.go.kr:8088/"
+        val API_KEY = "567657544a61736836326e76414457"
+    }
+}
+
+interface SeoulOpenService {
+    @GET("{api_key}/json/SeoulPublicLibraryInfo/1/203")
+    fun getLibrary(@Path("api_key") key:String) : Call<Library>
+}
