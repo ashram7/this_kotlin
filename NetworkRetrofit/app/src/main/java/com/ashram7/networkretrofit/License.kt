db1f08531ee0a531f1b046aeed4a123272b45793
ashram7 <ashram7@hanmail.net>
2022-11-04 11:23:06 +0900

    Newwork program

---
+++ NetworkRetrofit/app/src/main/java/com/ashram7/networkretrofit/License.kt
@@ -0,0 +1,9 @@
+package com.ashram7.networkretrofit
+
+data class License(
+    val key: String,
+    val name: String,
+    val node_id: String,
+    val spdx_id: String,
+    val url: String
+)
---
+++ NetworkRetrofit/app/src/main/java/com/ashram7/networkretrofit/Repository.kt
@@ -0,0 +1,3 @@
+package com.ashram7.networkretrofit
+
+class Repository : ArrayList<RepositoryItem>()
