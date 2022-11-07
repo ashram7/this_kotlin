400a2b66f8fce6e0f2a1ad5dfd9fe9ddffab1d81
ashram7 <ashram7@hanmail.net>
2022-11-07 12:35:33 +0900

    firebase

--- Firebase/app/src/main/java/com/ashram7/firebase/MainActivity.kt
+++ Firebase/app/src/main/java/com/ashram7/firebase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ashram7.firebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.ashram7.firebase.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -13,9 +14,11 @@ class MainActivity : AppCompatActivity() {
     val database = Firebase.database("https://this-is-android-with-kot-d7f5e-default-rtdb.asia-southeast1.firebasedatabase.app/")
     val myRef = database.getReference("users")
 
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         fun addItem(user:User) {
             val id = myRef.push().key!!
@@ -23,14 +26,30 @@ class MainActivity : AppCompatActivity() {
             myRef.child(id).setValue(user)
         }
 
-        myRef.child("name").addValueEventListener(object: ValueEventListener {
+        with(binding){
+            btnPost.setOnClickListener {
+                val name = editName.text.toString()
+                val password = editPassword.text.toString()
+                val user = User(name, password)
+                addItem(user)
+            }
+        }
+
+        myRef.addValueEventListener( object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                Log.d("파이어베이스", "${snapshot.value}")
+
+                binding.textList.setText("")
+                for (item in snapshot.children) {
+                    item.getValue(User::class.java)?.let { user ->
+                        binding.textList.append("${user.name} : ${user.password} \n")
+                    }
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
             }
         })
+
     }
 }
 
