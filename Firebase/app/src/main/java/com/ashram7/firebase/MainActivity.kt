f0b1e04db0fc321d706e8b402729cbec4fa3489f
ashram7 <ashram7@hanmail.net>
2022-11-07 11:31:41 +0900

    firebase

--- Firebase/app/src/main/java/com/ashram7/firebase/MainActivity.kt
+++ Firebase/app/src/main/java/com/ashram7/firebase/MainActivity.kt
@@ -2,16 +2,47 @@ package com.ashram7.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database("https://this-is-android-with-kot-d7f5e-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    val myRef = database.getReference("users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-        myRef.setValue("Hello, Firebase")
+        fun addItem(user:User) {
+            val id = myRef.push().key!!
+            user.id = id
+            myRef.child(id).setValue(user)
+        }
+
+        myRef.child("name").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("파이어베이스", "${snapshot.value}")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+}
+
+class User {
+    var id:String = ""
+    var name:String = ""
+    var password:String = ""
+
+    constructor() // 파이어베이스에서 데이터 변환을 위해서 필요
+
+    constructor(name:String, password:String) {
+        this.name = name
+        this.password = password
     }
 }
